35540000387aa2105071d23fffb4a3ba99e7b28a
Dasha Palshau <dasha.palshau@gmail.com>
2022-10-06 16:25:06 +0300

    Initial Commit

---
+++ GreenDataTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GreenDataTest
+//
+//  Created by Dasha Palshau on 06.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
