00ae44111eddb2961224f1e6b729700c2b7a6ac3
Dasha Palshau <dasha.palshau@gmail.com>
2022-10-08 20:20:33 +0300

    Data formatting for profile and UI adjusted for landscape orientation

--- GreenDataTest/Profile/ProfileViewController.swift
+++ GreenDataTest/Profile/ProfileViewController.swift
@@ -9,14 +9,48 @@ import UIKit
 
 class ProfileViewController: UIViewController {
     
-    private let userImageView = UIImageView()
-    private let nameLabel = UILabel()
+    private let userImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.backgroundColor = .systemGray6
+        imageView.clipsToBounds = true
+        imageView.layer.cornerRadius = 10
+        return imageView
+    }()
+    
+    private let loader = UIActivityIndicatorView()
+    private let nameLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 22, weight: .medium)
+        label.textColor = .label
+        return label
+    }()
+    
     private let genderIcon = UIImageView()
-    private let dateOfBirthLabel = UILabel()
-    private let emailLabel = UILabel()
-    private let localTimeLabel = UILabel()
+    private let dateOfBirthLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 16, weight: .medium)
+        label.textColor = .secondaryLabel
+        return label
+    }()
+    
+    private let emailLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 18)
+        label.textColor = .secondaryLabel
+        return label
+    }()
+    
+    private let localTimeLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 16, weight: .medium)
+        label.textColor = .secondaryLabel
+        return label
+    }()
     
     private let viewModel: ProfileViewModel
+    private var cancelLoading: (() -> Void)?
+    private var portraitConstraints = [NSLayoutConstraint]()
+    private var landscapeConstraints = [NSLayoutConstraint]()
     
     init(viewModel: ProfileViewModel) {
         self.viewModel = viewModel
@@ -27,18 +61,36 @@ class ProfileViewController: UIViewController {
         fatalError("init(coder:) has not been implemented")
     }
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = "Profile"
-        view.backgroundColor = .white
+        
+        view.backgroundColor = .systemBackground
+        portraitConstraints = constraintsForPortraitOrientation()
+        landscapeConstraints = constraintsForLandscapeOrientation()
         
         configureConstaints()
         configure(with: viewModel.viewModel())
     }
     
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        super.traitCollectionDidChange(previousTraitCollection)
+        activateConstaints(orientationIsPortrait:  UIDevice.current.orientation.isPortrait || !UIDevice.current.orientation.isLandscape)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        cancelLoading?()
+    }
+    
     func configure(with userProfile: ViewModel) {
-        userImageView.image = UIImage(systemName: "person.fill")
+        loader.startAnimating()
+        cancelLoading = viewModel.loadImage(url: userProfile.imageURL) { [weak self] image in
+            DispatchQueue.main.async {
+                self?.userImageView.image = image
+                self?.loader.stopAnimating()
+            }
+        }
+        
         nameLabel.text = userProfile.name
         genderIcon.image = userProfile.genderIcon
         dateOfBirthLabel.text = userProfile.dateOfBirth
@@ -47,16 +99,36 @@ class ProfileViewController: UIViewController {
     }
 
     private func configureConstaints() {
-        [userImageView, nameLabel, genderIcon, dateOfBirthLabel, emailLabel, localTimeLabel].forEach {
+        [userImageView, loader, nameLabel, genderIcon, dateOfBirthLabel, emailLabel, localTimeLabel].forEach {
             $0.translatesAutoresizingMaskIntoConstraints = false
             view.addSubview($0)
         }
         
-        NSLayoutConstraint.activate([
+        activateConstaints(orientationIsPortrait: UIDevice.current.orientation.isPortrait || !UIDevice.current.orientation.isLandscape)
+    }
+    
+    private func activateConstaints(orientationIsPortrait: Bool) {
+        NSLayoutConstraint.deactivate(portraitConstraints)
+        NSLayoutConstraint.deactivate(landscapeConstraints)
+        
+        if orientationIsPortrait {
+            NSLayoutConstraint.activate(portraitConstraints)
+        } else {
+            NSLayoutConstraint.activate(landscapeConstraints)
+        }
+        
+        view.layoutIfNeeded()
+    }
+    
+    private func constraintsForPortraitOrientation() -> [NSLayoutConstraint] {
+        [
             userImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             userImageView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
             userImageView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
             userImageView.heightAnchor.constraint(equalTo: userImageView.widthAnchor),
+            
+            loader.centerYAnchor.constraint(equalTo: userImageView.centerYAnchor),
+            loader.centerXAnchor.constraint(equalTo: userImageView.centerXAnchor),
             
             nameLabel.topAnchor.constraint(equalTo: userImageView.bottomAnchor, constant: 16),
             nameLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
@@ -64,16 +136,44 @@ class ProfileViewController: UIViewController {
             genderIcon.centerYAnchor.constraint(equalTo: nameLabel.centerYAnchor),
             genderIcon.leadingAnchor.constraint(equalTo: nameLabel.trailingAnchor, constant: 8),
             
-            dateOfBirthLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 8),
-            dateOfBirthLabel.leadingAnchor.constraint(equalTo: nameLabel.leadingAnchor),
-            dateOfBirthLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
-            
-            emailLabel.topAnchor.constraint(equalTo: dateOfBirthLabel.bottomAnchor, constant: 8),
+            emailLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 4),
             emailLabel.leadingAnchor.constraint(equalTo: nameLabel.leadingAnchor),
+            emailLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
             
-            localTimeLabel.topAnchor.constraint(equalTo: emailLabel.bottomAnchor, constant: 8),
+            dateOfBirthLabel.topAnchor.constraint(equalTo: emailLabel.bottomAnchor, constant: 12),
+            dateOfBirthLabel.leadingAnchor.constraint(equalTo: nameLabel.leadingAnchor),
+            
+            localTimeLabel.topAnchor.constraint(equalTo: dateOfBirthLabel.bottomAnchor, constant: 4),
             localTimeLabel.leadingAnchor.constraint(equalTo: nameLabel.leadingAnchor)
-        ])
+        ]
+    }
+    
+    private func constraintsForLandscapeOrientation() -> [NSLayoutConstraint] {
+        [
+            userImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            userImageView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            userImageView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            userImageView.widthAnchor.constraint(equalTo: userImageView.heightAnchor),
+            
+            loader.centerYAnchor.constraint(equalTo: userImageView.centerYAnchor),
+            loader.centerXAnchor.constraint(equalTo: userImageView.centerXAnchor),
+            
+            nameLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16),
+            nameLabel.leadingAnchor.constraint(equalTo: userImageView.trailingAnchor, constant: 16),
+            
+            genderIcon.centerYAnchor.constraint(equalTo: nameLabel.centerYAnchor),
+            genderIcon.leadingAnchor.constraint(equalTo: nameLabel.trailingAnchor, constant: 8),
+            
+            emailLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 4),
+            emailLabel.leadingAnchor.constraint(equalTo: nameLabel.leadingAnchor),
+            emailLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            
+            dateOfBirthLabel.topAnchor.constraint(equalTo: emailLabel.bottomAnchor, constant: 12),
+            dateOfBirthLabel.leadingAnchor.constraint(equalTo: nameLabel.leadingAnchor),
+            
+            localTimeLabel.topAnchor.constraint(equalTo: dateOfBirthLabel.bottomAnchor, constant: 4),
+            localTimeLabel.leadingAnchor.constraint(equalTo: nameLabel.leadingAnchor)
+        ]
     }
     
     struct ViewModel {
--- GreenDataTest/Profile/ProfileViewModel.swift
+++ GreenDataTest/Profile/ProfileViewModel.swift
@@ -11,22 +11,68 @@ import UIKit
 final class ProfileViewModel {
     
     var coordinator: ProfileCoordinator?
-    var user: RandomUser
+    private let user: RandomUser
+    private let api: RandomUserAPI
     
-    init(user: RandomUser) {
+    private lazy var formatter: DateFormatter = {
+            let formatter = DateFormatter()
+            formatter.calendar = Calendar(identifier: .iso8601)
+            formatter.locale = Locale(identifier: "en_US_POSIX")
+            formatter.timeZone = TimeZone(secondsFromGMT: 0)
+            formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSXXXXX"
+            return formatter
+        }()
+    
+    private lazy var toStringFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd.MM.yyyy"
+        return formatter
+    }()
+    
+    init(user: RandomUser, api: RandomUserAPI = RandomUserAPI()) {
         self.user = user
+        self.api = api
     }
     
     func viewModel() -> ProfileViewController.ViewModel {
-        let gender = user.gender == .female ? UIImage(systemName: "") : UIImage(systemName: "")
+        let gender = user.gender == .female ? "👩".image(ofSize: 32) : "👨".image(ofSize: 32)
+        let date = formatter.date(from: user.dateOfBirth.date)
+        var formattedDate = ""
+        let yearsOfAge = user.dateOfBirth.age % 10 == 1 ? "year" : "years"
+        
+        if let date = date {
+            formattedDate = toStringFormatter.string(from: date)
+        }
+        
         return ProfileViewController.ViewModel(
             imageURL: user.picture.large,
             name: user.name.first + " " + user.name.last,
             genderIcon: gender,
-            dateOfBirth: "\(user.dateOfBirth.date) (\(user.dateOfBirth.age) years)",
+            dateOfBirth: "Date of birth: \(formattedDate) (\(user.dateOfBirth.age) \(yearsOfAge))",
             email: user.email,
-            localTime: "\(Date()) \(user.location.timezone.offset)"
+            localTime: "Local time: \(Date()) \(user.location.timezone.offset)"
         )
     }
     
+    func loadImage(url: URL, completion: @escaping (UIImage?) -> Void) -> RandomUserAPI.Cancellable? {
+        return api.loadImage(url: url) { result in
+            switch result {
+            case .success(let image):
+                completion(image)
+            case .failure(_):
+                completion(nil)
+            }
+        }
+    }
+    
+}
+
+extension String {
+    func image(ofSize fontSize: CGFloat) -> UIImage? {
+        let size = CGSize(width: fontSize, height: fontSize)
+        let rect = CGRect(origin: CGPoint(), size: size)
+        return UIGraphicsImageRenderer(size: size).image { (context) in
+            (self as NSString).draw(in: rect, withAttributes: [.font : UIFont.systemFont(ofSize: fontSize - 8)])
+        }
+    }
 }
